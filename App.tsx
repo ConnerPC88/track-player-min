dc40124e9211fbbb8091df89c0628c43cbe9f6d0
Conner Christ <cchrist@sermonaudio.com>
2025-08-04 15:12:14 -0400

    new tracks

--- App.tsx
+++ App.tsx
@@ -13,16 +13,26 @@ import {
   View,
   Text,
 } from 'react-native';
-import { useDerivedValue } from 'react-native-reanimated';
 import TrackPlayer, {
   AppKilledPlaybackBehavior,
   Capability,
+  TrackType,
   useIsPlaying,
   useProgress,
+  Event,
 } from 'react-native-track-player';
 
 const setupPlayer = async () => {
   await TrackPlayer.setupPlayer({ autoHandleInterruptions: true });
+  TrackPlayer.addEventListener(Event.MetadataCommonReceived, data => {
+    console.log('track metadata:', data);
+  });
+  TrackPlayer.addEventListener(Event.MetadataChapterReceived, data => {
+    console.log('audio chapter metadata:', data);
+  });
+  TrackPlayer.addEventListener(Event.PlaybackError, data => {
+    console.log('playback error:', data);
+  });
   await TrackPlayer.updateOptions({
     capabilities: [
       Capability.Play,
@@ -65,24 +75,14 @@ function App() {
 
   useEffect(() => {
     TrackPlayer.load({
+      artist: 'Bill Parker',
       artwork:
-        'https://vps.sermonaudio.com/resize_image/speakers/podcast/600/600/10522-0001.jpg',
-      broadcasterArtwork:
-        'https://vps.sermonaudio.com/resize_image/sources/podcast/600/600/gcc2.jpg',
-      broadcasterID: 'gcc',
-      broadcasterName: 'Grace Community Church',
-      duration: 2856,
-      fromAutoPlay: false,
-      id: '81251442472460',
-      isLive: false,
-      isVideo: false,
-      roundedSpeakerImage:
-        'https://media.sermonaudio.com/images/speakers/thumbnail/10522-0001.png',
-      seriesID: 178120,
-      seriesTitle: 'Mark',
-      speaker: 'Craig Mussulman',
-      title: 'Kingdom Economics',
-      url: 'https://cloud.sermonaudio.net/media/audio/high/81251442472460.mp3?ts=1754060303&language=eng',
+        'https://vps.sermonaudio.com/resize_image/speakers/podcast/600/600/7463-0002.jpg',
+      duration: 1871,
+      id: '129241320364515',
+      title: 'Real Faith',
+      type: TrackType.Default,
+      url: 'https://cloud.sermonaudio.net/media/audio/high/129241320364515.mp3?ts=1733754717&language=eng',
     });
     TrackPlayer.play();
   }, []);
@@ -112,26 +112,88 @@ function App() {
       .substring(duration < 3600 ? 14 : 11, 19);
   }, [duration]);
 
+  const loadOriginalTrack = async () => {
+    await TrackPlayer.reset();
+    await TrackPlayer.load({
+      artist: 'Bill Parker',
+      artwork:
+        'https://vps.sermonaudio.com/resize_image/speakers/podcast/600/600/7463-0002.jpg',
+      duration: 1871,
+      id: '129241320364515',
+      title: 'Real Faith',
+      type: TrackType.Default,
+      url: 'https://cloud.sermonaudio.net/media/audio/high/129241320364515.mp3?ts=1733754717&language=eng',
+    });
+    await TrackPlayer.play();
+  };
+
+  const loadNewTrack = async () => {
+    await TrackPlayer.reset();
+    await TrackPlayer.load({
+      artist: 'Douglas Salyer',
+      artwork:
+        'https://vps.sermonaudio.com/resize_image/speakers/podcast/600/600/22398-0002.jpg',
+      duration: 3203,
+      id: '8425173741181',
+      title: 'The Vision of Dry Bones',
+      type: TrackType.Default,
+      url: 'https://cloud.sermonaudio.net/media/audio/high/8425173741181.mp3?ts=1754331195&language=eng',
+    });
+    await TrackPlayer.play();
+  };
+
+  const loadNewTestTrack = async () => {
+    await TrackPlayer.reset();
+    await TrackPlayer.load({
+      artist: 'Test',
+      duration: 42,
+      id: '123456',
+      title: 'Test Audio',
+      type: TrackType.Default,
+      url: 'https://file-examples.com/storage/fe180a8b03688f5559b9baf/2017/11/file_example_MP3_700KB.mp3',
+    });
+    await TrackPlayer.play();
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: 700,
-        }}
-      >
-        <Text style={{ color: 'red', fontSize: 32 }}>{timestampStart}</Text>
+      <View style={{ height: 700 }}>
         <Text
-          style={{ color: 'red', fontSize: 32, marginHorizontal: 10 }}
-          onPress={togglePlay}
+          style={{ marginTop: 150, alignSelf: 'center' }}
+          onPress={loadOriginalTrack}
         >
-          {playPauseText}
+          Load original audio track
         </Text>
-        <Text style={{ color: 'red', fontSize: 32 }}>{timestampEnd}</Text>
+        <Text
+          style={{ marginTop: 150, alignSelf: 'center' }}
+          onPress={loadNewTrack}
+        >
+          Load new audio track
+        </Text>
+        <Text
+          style={{ marginTop: 150, alignSelf: 'center' }}
+          onPress={loadNewTestTrack}
+        >
+          Load new test track
+        </Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text style={{ color: 'red', fontSize: 32 }}>{timestampStart}</Text>
+          <Text
+            style={{ color: 'red', fontSize: 32, marginHorizontal: 10 }}
+            onPress={togglePlay}
+          >
+            {playPauseText}
+          </Text>
+          <Text style={{ color: 'red', fontSize: 32 }}>{timestampEnd}</Text>
+        </View>
       </View>
     </View>
   );
